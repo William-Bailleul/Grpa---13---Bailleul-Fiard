c5841d1ff6c65b48a04d35bfbfad2682d7362c93
William Bailleul <wbailleul@gaming.tech>
2022-11-30 16:22:04 +0100

    d'accord

---
+++ components/navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="index.php" class="brand-logo center"><i class="material-icons">videogame_asset</i>Syphon</a>
+          <div class="right">
+            <i class="material-icons dropdown-trigger" id="modeswitcher"> remove_red_eye</i>
+          </div>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="left hide-on-med-and-down">
+            <li><a href="terraria.php">Terraria</a></li>
+            <li><a href="dbd.php">Dead By Daylight</a></li>
+            <li><a href="soom.php">Soom</a></li>
+            <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <ul class="sidenav" id="mobile-demo">
+        <li><a href="terraria.php">Terraria</a></li>
+        <li><a href="dbd.php">Dead By Daylight</a></li>
+        <li><a href="soom.php">Soom</a></li>
+        <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
+        <li><i class="fa-solid fa-cat" id="nyan"></i></li>
+      </ul>
+    </header>
+  </body>
+</html>
