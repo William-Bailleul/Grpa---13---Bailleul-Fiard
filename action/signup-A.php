6cb6d8f1304e4bce9e5b23fd932ed2606b9b6b10
William Bailleul <wbailleul@gaming.tech>
2022-11-30 21:26:16 +0100

    fe

--- action/signup-A.php
+++ action/signup-A.php
@@ -1,6 +1,21 @@
+<!DOCTYPE html>
 <html lang="fr">
-
 <head>
+  <meta charset="utf-8">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link id="theme-controller" type="text/css" rel="stylesheet" href="css/theme-dark.css">
+  <link type="text/css" rel="stylesheet" href="css/materialize.css">
+  <link type="text/css" rel="stylesheet" href="css/script.css">
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+  <link rel="icon" href="img/syphon.jpg">
+  <title>S'inscrire</title>
 </head>
 
 <?php
