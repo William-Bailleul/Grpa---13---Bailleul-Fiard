4d66acb964ba7334c757ea8a05d0a7d45c2a9d4d
Loris Fiard <lfiard@gaming.tech>
2022-11-30 20:53:22 +0100

    dsq

--- signup.php
+++ signup.php
@@ -21,32 +21,7 @@
 
  <body>
 
-   <header>
-     <!-- nav bar -->
-     <nav>
-       <div class="nav-wrapper">
-         <a href="index.php" class="brand-logo center"><i class="material-icons">videogame_asset</i>Syphon</a>
-         <div class="right">
-           <i class="material-icons dropdown-trigger" id="modeswitcher"> remove_red_eye</i>
-         </div>
-         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-         <ul class="left hide-on-med-and-down">
-           <li><a href="terraria.php">Terraria</a></li>
-           <li><a href="dbd.php">Dead By Daylight</a></li>
-           <li><a href="soom.php">Soom</a></li>
-           <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
-         </ul>
-       </div>
-     </nav>
-     <ul class="sidenav" id="mobile-demo">
-       <li><a href="terraria.php">Terraria</a></li>
-       <li><a href="dbd.php">Dead By Daylight</a></li>
-       <li><a href="soom.php">Soom</a></li>
-       <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
-       <li><i class="fa-solid fa-cat" id="nyan"></i></li>
-     </ul>
-   </header>
-
+  <?php require "components/navbar.php" ?>
 
    <div class="block1">
      <div class="row animate__animated animate__fadeIn wow">
