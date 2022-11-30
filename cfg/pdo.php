f2dd1a3b9f4e9eacdb96a71af9336cb5028e8f4f
Loris Fiard <lfiard@gaming.tech>
2022-11-30 21:20:14 +0100

    rerara

--- cfg/pdo.php
+++ cfg/pdo.php
@@ -1,3 +1,5 @@
+<?php
+
 $pdo = new PDO(
 	'mysql:host=localhost;dbname=mybdd;',
 	'root',
@@ -5,3 +7,5 @@ $pdo = new PDO(
 	array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+?>
--- signin.php
+++ signin.php
@@ -43,7 +43,45 @@
     </div>
   </div>
 
-
+  <!-- Modal -->
+  <div class="modal-content">
+    <div id="modal1" class="modal modal-fixed-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 l12">
+            <h5 class="blue-text">Contact</h5>
+            <div class="row">
+              <form class="col s12 l12">
+                <div class="row">
+                  <div class="input-field col s6 l6">
+                    <input id="first_name" type="text" class="validate">
+                    <label for="first_name">Prénom</label>
+                  </div>
+                  <div class="input-field col s6 l6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Nom</label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="input-field col s12 l12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                  </div>
+                  <div class="input-field col s12 l12">
+                    <textarea id="textarea1" class="materialize-textarea"></textarea>
+                    <label for="textarea1">Message</label>
+                    <div class="modal-footer">
+                      <a href="#!" class="modal-close waves-effect waves-light btn-flat">Agree</a>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <!--JavaScript at end of body for optimized loading-->
   <script src="js/jquery.js"></script>
   <script src="js/materialize.js"></script>
