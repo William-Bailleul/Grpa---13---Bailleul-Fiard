ba5408cb681d54b978111c5464c36edb4545fe92
William Bailleul <wbailleul@gaming.tech>
2022-11-30 22:01:58 +0100

    d

--- components/footer.php
+++ components/footer.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
   </head>
   <body>
-
     <!-- Footer -->
     <footer class="page-footer">
       <div class="container">
@@ -42,12 +41,12 @@
     </footer>
 
   <!--JavaScript at end of body for optimized loading-->
-  <script src="js/jquery.js"></script>
-  <script src="js/materialize.js"></script>
+  <script src="../js/jquery.js"></script>
+  <script src="../js/materialize.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
     integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="js/script.js"></script>
+  <script src="../js/script.js"></script>
   <script src="https://kit.fontawesome.com/198411c19a.js" crossorigin="anonymous"></script>
   </body>
 </html>
