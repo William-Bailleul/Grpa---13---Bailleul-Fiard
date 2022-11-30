e78313006d8e2f5a042dcaaf1b4458561f2ce1e3
Loris Fiard <lfiard@gaming.tech>
2022-11-30 12:55:33 +0100

    g

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
    <form method="post" action="signup.php">
        <input type='email' name='email' placeholder="" />
        <input type='password' name='password' />
-       <input type='text' name='login' />
+       <input type='username' name='username' />
        <input type='submit' value='Me connecter' />
    </form>
  </body>
