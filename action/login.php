a93aa4b072672c48813ab48df49187104c1c4aa2
William Bailleul <wbailleul@gaming.tech>
2022-11-30 21:43:23 +0100

    y

--- action/login.php
+++ action/login.php
@@ -33,16 +33,6 @@
       </div>
      </div>
     </div>
-<<<<<<< HEAD
     <?php require "components/footer.php" ?>
-=======
-
-    <!--JavaScript at end of body for optimized loading-->
-    <script src="js/jquery.js"></script>
-    <script src="js/materialize.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="js/script.js"></script>
-    <script src="https://kit.fontawesome.com/198411c19a.js" crossorigin="anonymous"></script>
->>>>>>> 68d2930c9eb5838d6201f067e39e897730537090
  </body>
 </html>
