cd5f3c04498d18f0d11febfe7d485564d6b9b4cd
Loris Fiard <lfiard@gaming.tech>
2022-11-30 17:03:46 +0100

    Update login.php

--- action/login.php
+++ action/login.php
@@ -1,12 +1,68 @@
 <html>
  <head>
+   <meta charset="utf-8">
+   <!--Import Google Icon Font-->
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+   <!--Import materialize.css-->
+   <link id="theme-controller" type="text/css" rel="stylesheet" href="../css/theme-dark.css">
+   <link type="text/css" rel="stylesheet" href="../css/materialize.css">
+   <link type="text/css" rel="stylesheet" href="../css/script.css">
+   <!--Let browser know website is optimized for mobile-->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+   <link rel="icon" href="../img/syphon.jpg">
+   <title>Syphon Games</title>
  </head>
  <body>
-   <form method="post" action="signup.php">
-       <input type='email' name='email' placeholder="" />
-       <input type='password' name='password' />
-       <input type='username' name='username' />
-       <input type='submit' value='Me connecter' />
-   </form>
+   <header>
+     <!-- nav bar -->
+     <nav>
+       <div class="nav-wrapper">
+         <a href="../index.php" class="brand-logo center"><i class="material-icons">videogame_asset</i>Syphon</a>
+         <div class="right">
+           <i class="material-icons dropdown-trigger" id="modeswitcher"> remove_red_eye</i>
+         </div>
+         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+         <ul class="left hide-on-med-and-down">
+           <li><a href="../terraria.php">Terraria</a></li>
+           <li><a href="../dbd.php">Dead By Daylight</a></li>
+           <li><a href="../soom.php">Soom</a></li>
+           <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
+         </ul>
+       </div>
+     </nav>
+     <ul class="sidenav" id="mobile-demo">
+       <li><a href="../terraria.php">Terraria</a></li>
+       <li><a href="../dbd.php">Dead By Daylight</a></li>
+       <li><a href="../soom.php">Soom</a></li>
+       <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
+       <li><i class="fa-solid fa-cat" id="nyan"></i></li>
+     </ul>
+   </header>
+
+
+   <div class="block1">
+     <div class="row animate__animated animate__fadeIn wow">
+       <div class="col s10 offset-s1 l6 offset-l3 description anim1">
+         <div class="center gradient">
+           <form method="post" action="signup.php">
+             <input type='email' name='email' placeholder="email" /> <br>
+             <input type='password' name='password' placeholder="mot de passe" /> <br>
+             <input type='text' name='username' placeholder="nom d'utilisateur" /> <br>
+             <input type='text' name='country' placeholder="country" /> <br>
+             <input type='number' name='phone_number' placeholder="numéro de téléphone" /> <br>
+             <input type='date' name='birth_date' placeholder="birth date" /> <br>
+             <input type='submit' value='Me connecter' />
+           </form>
+
+        </div>
+      </div>
+     </div>
+    </div>
+
+
  </body>
 </html>
