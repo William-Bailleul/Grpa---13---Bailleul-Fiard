de59cb7819b9b5ef384edd5cdbd1046dc645a293
Loris Fiard <lfiard@gaming.tech>
2022-11-30 18:13:34 +0100

    Update and rename login.php to signin.php

--- action/signin.php
+++ action/signin.php
@@ -1,5 +1,5 @@
 <html>
- 
+
  <head>
    <meta charset="utf-8">
    <!--Import Google Icon Font-->
@@ -15,11 +15,12 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
    <link rel="icon" href="../img/syphon.jpg">
-   <title>Syphon Games</title>
+   <title>Se connecter</title>
+
  </head>
- 
+
  <body>
-  
+
    <header>
      <!-- nav bar -->
      <nav>
@@ -52,18 +53,63 @@
        <div class="col s10 offset-s1 l6 offset-l3 description anim1">
          <div class="center gradient">
            <form method="post" action="signup.php">
-             <input type='email' name='email' placeholder="email" /> <br>
-             <input type='password' name='password' placeholder="mot de passe" /> <br>
              <input type='text' name='username' placeholder="nom d'utilisateur" /> <br>
-             <input type='text' name='country' placeholder="country" /> <br>
-             <input type='number' name='phone_number' placeholder="numéro de téléphone" /> <br>
-             <input type='date' name='birth_date' placeholder="birth date" /> <br>
-             <input type='submit' value='Me connecter' />
+             <input type='password' name='password' placeholder="mot de passe" /> <br>
+
+             <button class="btn waves-effect waves-light" type="submit" name="action">TEST
+               <i class="material-icons right">TEST</i>
+             </button>
+
            </form>
         </div>
       </div>
      </div>
     </div>
 
+    <!-- Modal -->
+    <div class="modal-content">
+      <div id="modal1" class="modal modal-fixed-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col s12 l12">
+              <h5 class="blue-text">Contact</h5>
+              <div class="row">
+                <form class="col s12 l12">
+                  <div class="row">
+                    <div class="input-field col s6 l6">
+                      <input id="first_name" type="text" class="validate">
+                      <label for="first_name">Prénom</label>
+                    </div>
+                    <div class="input-field col s6 l6">
+                      <input id="last_name" type="text" class="validate">
+                      <label for="last_name">Nom</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12 l12">
+                      <input id="email" type="email" class="validate">
+                      <label for="email">Email</label>
+                    </div>
+                    <div class="input-field col s12 l12">
+                      <textarea id="textarea1" class="materialize-textarea"></textarea>
+                      <label for="textarea1">Message</label>
+                      <div class="modal-footer">
+                        <a href="#!" class="modal-close waves-effect waves-light btn-flat">Agree</a>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--JavaScript at end of body for optimized loading-->
+    <script src="../js/jquery.js"></script>
+    <script src="../js/materialize.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="../js/script.js"></script>
+    <script src="https://kit.fontawesome.com/198411c19a.js" crossorigin="anonymous"></script>
  </body>
 </html>
