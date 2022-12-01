8274c7592e411f738d181027028d888d3441429a
Bailleul William <wbailleul@gaming.tech>
2022-12-01 12:34:50 +0100

    s

---
+++ components/js.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <script src="js/jquery.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"
+      integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g=="
+      crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="js/script.js"></script>
+    <script src="https://kit.fontawesome.com/198411c19a.js" crossorigin="anonymous"></script>
+  </body>
+</html>
