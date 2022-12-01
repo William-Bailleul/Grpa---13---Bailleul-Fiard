ec0a2249d02e44373910270ad2ffd1d97ef8ed1e
William Bailleul <wbailleul@gaming.tech>
2022-12-01 21:12:24 +0100

    k

--- action/signin-action.php
+++ action/signin-action.php
@@ -11,7 +11,8 @@ $user = $pre->fetchall(PDO::FETCH_ASSOC);
 if(empty($user)){ //vérifie si le resultat est vide !
      //non connecté
      echo "Utilisateur ou mot de passe incorrect !";
-     print_r("oups"); exit() ;
+     header('Location:..signup.php');
+     exit() ;
 }else{
      $_SESSION['user'] = $user[0]; //on enregistre que l'utilisateur est connecté
      print_r($_SESSION['username']); exit() ;
