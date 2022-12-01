914eb759182d49d45c829569a6d4ab4286cee4a0
Loris Fiard <lfiard@gaming.tech>
2022-12-01 18:24:44 +0100

    dsqd

--- index.php
+++ index.php
@@ -19,32 +19,7 @@
 </head>
 <body>
   <header>
-    <!-- nav bar -->
-    <nav>
-      <div class="nav-wrapper">
-        <a href="index.php" class="brand-logo center"><i class="material-icons">videogame_asset</i>Syphon</a>
-        <div class="right">
-          <i class="fa-regular fa-circle-user"></i>
-          <!--<i class="fa-solid fa-circle-user"></i>-->
-          <i class="material-icons dropdown-trigger" id="modeswitcher"> remove_red_eye</i>
-        </div>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul class="left hide-on-med-and-down">
-          <li><a href="terraria.php">Terraria</a></li>
-          <li><a href="dbd.php">Dead By Daylight</a></li>
-          <li><a href="soom.php">Soom</a></li>
-          <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
-        </ul>
-      </div>
-    </nav>
-    <ul class="sidenav" id="mobile-demo">
-      <li><a href="terraria.php">Terraria</a></li>
-      <li><a href="dbd.php">Dead By Daylight</a></li>
-      <li><a href="soom.php">Soom</a></li>
-      <li><a class="waves-effect waves-light btn modal-trigger blue2" href="#modal1">Contact</a></li>
-      <li><i class="fa-solid fa-cat" id="nyan"></i></li>
-    </ul>
-  </header>
+  <?php require "components/navbar.php"?>
   <!-- Carousel -->
   <div class="row">
     <div class="col s12 l8 offset-l2">
